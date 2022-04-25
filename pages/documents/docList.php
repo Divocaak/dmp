bec270b95727208bdac4e995cf93982819f6ddce
Vojtěch Divoký <divokyvojta@gmail.com>
2022-04-25 23:58:53 +0200

    note

--- pages/documents/docList.php
+++ pages/documents/docList.php
@@ -57,6 +57,7 @@ if ($result = mysqli_query($link, $sql)) {
             unset($_SESSION["documents"]);
             for ($i = 0; $i < count($documents); $i++) {
                 $_SESSION["documents"][$documents[$i]["id"]] = $documents[$i];
+                // TODO download doc
                 echo '<tr>
                     <th scope="row">' . ($i + 1) . '</th>
                     <td>' . $documents[$i]["label"] . '</td>
