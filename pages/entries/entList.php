26412237e0b6842aa56fdd314b43e2d0c70d2400
Vojtěch Divoký <divokyvojta@gmail.com>
2022-06-09 18:38:18 +0200

    edit ent done, add ent wip

--- pages/entries/entList.php
+++ pages/entries/entList.php
@@ -67,40 +67,42 @@ session_start();
     $month = 5;
     $year = 2022;
 
-    $sql = "SELECT e.id, e.date, e.minutes, def.value, def.color, c.id, c.max_hours, c.max_cash, c.note, d.label, d.date_start, d.date_end, d.cash_rate 
+    $sql = "SELECT e.id, e.date, e.minutes, def.name, def.value, def.color, c.id, c.max_hours, c.max_cash, c.note, d.label, d.date_start, d.date_end, d.cash_rate 
             FROM entry e LEFT JOIN defaults def ON e.id_category=def.name INNER JOIN contract c ON e.id_contract=c.id INNER JOIN document d ON c.id_document=d.id
             WHERE YEAR(e.date)=" . $year . " AND MONTH(e.date)=" . $month . ";";
     if ($result = mysqli_query($link, $sql)) {
         $entries = [];
         $contracts = [];
         while ($row = mysqli_fetch_row($result)) {
-            $entries[] = [
+            $entries[$row[0]] = [
                 "id" => $row[0],
                 "date" => $row[1],
                 "minutes" => $row[2],
                 "tag" => [
-                    "label" => $row[3],
-                    "color" => $row[4]
+                    "id" => $row[3],
+                    "label" => $row[4],
+                    "color" => $row[5]
                 ],
                 "contract" => [
-                    "id" => $row[5],
-                    "maxHours" => $row[6],
-                    "maxCash" => $row[7],
-                    "note" => $row[8]
+                    "id" => $row[6],
+                    "maxHours" => $row[7],
+                    "maxCash" => $row[8],
+                    "note" => $row[9]
                 ],
                 "document" => [
-                    "label" => $row[9],
-                    "start" => $row[10],
-                    "end" => $row[11],
-                    "cashRate" => $row[12]
+                    "label" => $row[10],
+                    "start" => $row[11],
+                    "end" => $row[12],
+                    "cashRate" => $row[13]
                 ]
             ];
 
-            if (!in_array($row[5], $contracts)) {
-                $contracts[] = $row[5];
+            if (!in_array($row[6], $contracts)) {
+                $contracts[] = $row[6];
             }
         }
         mysqli_free_result($result);
+        $_SESSION["entries"] = $entries;
     } else {
         echo "eee";
     }
@@ -129,10 +131,10 @@ session_start();
                     $hasEntry = false;
 
                     $cellTag .= "<td data-day='" . $day . "' data-contract-id='" . $contract . "'";
-                    foreach ($entries as $entry) {
+                    foreach ($entries as $entryId => $entry) {
                         if (DateTime::createFromFormat("Y-m-d", $entry["date"])->format("d") == $day && $entry["contract"]["id"] == $contract) {
                             $hasEntry = true;
-                            $cellTag .= " data-entry-id='" . $entry["id"] . "'";
+                            $cellTag .= " data-entry-id='" . $entryId . "'";
                             $cellContent .= date('H:i', mktime(0, $entry["minutes"]));
                             $cellContent .= (isset($entry["tag"]["label"]) ? ('<span class="ms-2 badge rounded-pill" style="background-color:#' . $entry["tag"]["color"] . ';">' . $entry["tag"]["label"] . "</span>") : "") . "<br>";
                             $cellContent .= '<a class="mt-2 me-2 btn btn-outline-primary editBtn actionIdBtn"><i class="bi bi-pencil"></i></a>';
@@ -175,28 +177,35 @@ session_start();
                     <h5 class="modal-title" id="exampleModalLabel">Zápis</h5>
                 </div>
                 <div class="modal-body">
-                    <form class="needs-validation" novalidate action="" method="post">
+                    <form class="needs-validation" novalidate action="editEntScript.php" method="post" id="entFormHeader">
                         <div class="row">
                             <div class="col">
-                                <div class="form-floating mb-3">
+                                <div class="form-floating mb-3 p-2">
+                                    <input type="text" class="form-control" id="id" name="id" readonly value="">
+                                    <label for="id">ID</label>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-floating mb-3 p-2">
                                     <input type="number" class="form-control" id="hours" name="hours" required value="">
                                     <label for="hours">Hodiny</label>
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="form-floating mb-3">
+                                <div class="form-floating mb-3 p-2">
                                     <input type="number" class="form-control" id="minutes" name="minutes" required value="">
                                     <label for="minutes">Minuty</label>
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="form-floating mb-3">
-                                    <select class="form-select form-control" id="tag" name="tag">
+                                <div class="form-floating mb-3 p-2 rounded" id="selectedTagIndicator" style="background-color: transparent;">
+                                    <select class="form-select form-control" id="tagSelect" name="tagSelect">
                                         <?php
                                         $sql = "SELECT name, value, color FROM defaults WHERE status=1 AND color IS NOT NULL;";
                                         if ($result = mysqli_query($link, $sql)) {
+                                            echo "<option value='NULL' data-color='FFFFFF' selected>-</option>";
                                             while ($row = mysqli_fetch_row($result)) {
-                                                echo "<option value=" . $row[0] . "><span class='badge rounded-pill' style='background-color:#" . $row[2] . ";'>" . $row[1] . "</span></option>";
+                                                echo "<option value=" . $row[0] . " data-color='" . $row[2] . "'>" . $row[1] . "</option>";
                                             }
                                             mysqli_free_result($result);
                                         } else {
@@ -204,7 +213,7 @@ session_start();
                                         }
                                         ?>
                                     </select>
-                                    <label for="tag">Značka</label>
+                                    <label for="tagSelect">Značka</label>
                                 </div>
                             </div>
                         </div>
@@ -230,13 +239,13 @@ session_start();
                 showEntryForm(false);
             });
 
-            $(".editBtn").click(function() {
-                showEntryForm(true);
-            });
-
             var entId;
             $(".actionIdBtn").click(function() {
                 entId = $(this).parent().data("entryId");
+            });
+
+            $(".editBtn").click(function() {
+                showEntryForm(true, entId);
             });
 
             $(".deleteBtn").click(function() {
@@ -246,13 +255,37 @@ session_start();
             $("#confDeleteBtn").click(function() {
                 window.location = "delEntScript?id=" + entId;
             });
+
+            $("#tagSelect").change(function() {
+                $("#selectedTagIndicator").css('background-color', ("#" + $("#tagSelect option:selected").data("color")));
+            });
         });
 
-        function showEntryForm(isEdit) {
+        function showEntryForm(isEdit, entId) {
             $("#entryFormSaveBtn").attr('class', ("btn btn-outline-" + (isEdit ? "primary" : "success")));
             $("#entryFormSaveBtn").text(isEdit ? "Uložit" : "Přidat");
+            $("#entFormHeader").attr("action", ((isEdit ? "edit" : "add") + "EntScript.php"));
 
+            if(isEdit){
+                $.post("getEntData.php", {
+                    index: entId
+                }, function(data) {
+                    var dataDecoded = JSON.parse(data);
+                    entFormValues(dataDecoded["id"], Math.floor(dataDecoded["minutes"] / 60), (dataDecoded["minutes"] % 60), dataDecoded["tag"]["id"])
+                });
+            }else{
+                entFormValues("-", "", "", "NULL");
+            }
+            
             $('#entryFormModal').modal('show');
+        }
+        
+        function entFormValues(id, hours, minutes, tagId){
+            $("#id").val(id);
+            $("#hours").val(hours);
+            $("#minutes").val(minutes);
+            $("#tagSelect").val(tagId);
+            $("#selectedTagIndicator").css('background-color', ("#" + $("#tagSelect option:selected").data("color")));
         }
     </script>
 </body>
